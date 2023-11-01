91f3dec579e30340448af3dc493c685e37de6124
RRR <richardmacus0@gmail.com>
2023-11-01 21:17:39 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [userInput, setUserInput] = useState('');
+  const [chatLog, setChatLog] = useState([]);
+
+  const handleUserInput = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const handleSendMessage = async () => {
+    if (userInput.trim() === '') {
+      return;
+    }
+
+    // Add the user's message to the chat log
+    const newUserMessage = { text: userInput, isUser: true };
+    setChatLog((prevChatLog) => [...prevChatLog, newUserMessage]);
+
+    // Clear the user input field
+    setUserInput('');
+
+    try {
+      const response = await fetch('http://localhost:5000/chatbot', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message: userInput }),
+      });
+
+      const data = await response.json();
+      const chatbotResponse = data.message;
+
+      // Add the chatbot's response to the chat log
+      const newChatbotMessage = { text: chatbotResponse, isUser: false };
+      setChatLog((prevChatLog) => [...prevChatLog, newChatbotMessage]);
+    } catch (error) {
+      console.error('Error sending message:', error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="chat-window">
+        {chatLog.map((message, index) => (
+          <div key={index} className={`message ${message.isUser ? 'user' : 'chatbot'}`}>
+            {message.text}
+          </div>
+        ))}
+      </div>
+      <div className="user-input">
+        <textarea
+          placeholder="Type your message..."
+          value={userInput}
+          onChange={handleUserInput}
+        />
+        <button onClick={handleSendMessage}>Send</button>
+      </div>
     </div>
   );
 }
